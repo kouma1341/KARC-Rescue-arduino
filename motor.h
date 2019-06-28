2d720bdc0c4be0ac4c2c9c5b2e7d0bfef419e3c9
十六夜 紅魔 <37430389+kouma1341@users.noreply.github.com>
2019-06-28 17:49:32 +0900

    Update motor.h

--- motor.h
+++ motor.h
@@ -1,23 +1,16 @@
 #ifndef motor_h
 #define motor_h
 
-/* #define AIN1 13
-#define AIN2 12
-#define PWMA 11
-#define BIN1 10
-#define BIN2 9
-#define PWMB 8 */
-
 #include "Arduino.h"
 
 class Motor
 {
 public:
-  Motor(int aAIN1,int aAIN2,int aPWMA,int aBIN1,int aBIN2,int aPWMB,int bAIN1,int bAIN2,int bPWMA,int bBIN1,int bBIN2,int bPWMB); 
+  Motor(uint8_t aAIN1,uint8_t aAIN2,uint8_t aPWMA,uint8_t aBIN1,uint8_t aBIN2,uint8_t aPWMB,uint8_t bAIN1,uint8_t bAIN2,uint8_t bPWMA,uint8_t bBIN1,uint8_t bBIN2,uint8_t bPWMB);
   void mc(int m1,int m2,int m3,int m4,int del);
 
 private:
-  int _pin;
+  uint8_t _aAIN1,_aAIN2,_aPWMA,_aBIN1,_aBIN2,_aPWMB,_bAIN1,_bAIN2,_bPWMA,_bBIN1,_bBIN2,_bPWMB;
 };
 
 #endif
