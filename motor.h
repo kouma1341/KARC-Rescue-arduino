13e2283acab9fa303f2785639e0794aba61cef33
十六夜 紅魔 <37430389+kouma1341@users.noreply.github.com>
2019-07-11 17:49:49 +0900

    Update motor.h
    uint8_t →　int

--- motor.h
+++ motor.h
@@ -5,12 +5,12 @@
 
 class Motor
 {
-public:
-  Motor(uint8_t aAIN1,uint8_t aAIN2,uint8_t aPWMA,uint8_t aBIN1,uint8_t aBIN2,uint8_t aPWMB,uint8_t bAIN1,uint8_t bAIN2,uint8_t bPWMA,uint8_t bBIN1,uint8_t bBIN2,uint8_t bPWMB);
-  void mc(int m1,int m2,int m3,int m4,int del);
+  public:
+    Motor(int 1IN1, int 1IN2, int 1PWM, int 2IN1, int 2IN2, int 2PWM, int 3IN1, int 3IN2, int 3PWM, int 4IN1, int 4IN2, int 4PWM);
+    void mc(int m1, int m2, int m3, int m4, int del);
 
-private:
-  uint8_t _aAIN1,_aAIN2,_aPWMA,_aBIN1,_aBIN2,_aPWMB,_bAIN1,_bAIN2,_bPWMA,_bBIN1,_bBIN2,_bPWMB;
+  private:
+    int _1IN1, _1IN2, _1PWM, _2IN1, _2IN2, _2PWM, _3IN1, _3IN2, _3PWM, _4IN1, _4IN2, _4PWM;
 };
 
 #endif
